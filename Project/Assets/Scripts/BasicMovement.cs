aa7fc9669338d01b37a7336b631e659204033fc3
gstirlingAC <gordon.stirling@ayrshire.ac.uk>
2017-05-15 00:04:09 +0100

    Tutorial_2 Introduction to Navmesh Navigation

---
+++ Project/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicMovement : MonoBehaviour {
+
+    [SerializeField] NavMeshAgent navMeshAgent;
+    [SerializeField] Transform targetObject;
+
+    private void Awake ()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        targetObject = GameObject.Find("MoveTarget_1").transform;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        navMeshAgent.destination = targetObject.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
