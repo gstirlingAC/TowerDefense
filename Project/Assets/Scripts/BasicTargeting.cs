687f9adc330f7a76da02aff4e43c737bca67bb11
gstirlingAC <gordon.stirling@ayrshire.ac.uk>
2017-05-15 08:28:39 +0100

    Tutorial_3
    In this tutorial we will create a turret object using Blender.  We then import this turret object into Unity, and then we will create a new C# script to handle basic targeting of an enemy.

---
+++ Project/Assets/Scripts/BasicTargeting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargeting : MonoBehaviour {
+
+    [SerializeField] Transform target;
+
+    private void Awake()
+    {
+        target = GameObject.Find("Enemy").transform;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.LookAt(target);
+	}
+}
